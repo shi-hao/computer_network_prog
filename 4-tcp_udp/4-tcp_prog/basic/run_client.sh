4998841528ee32d99b60498c7337ede4d52af8ff
bleach <shi7631470@163.com>
2019-07-09 14:43:08 +0800

    add

--- 4-tcp_udp/4-tcp_prog/basic/run_client.sh
+++ 4-tcp_udp/4-tcp_prog/basic/run_client.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 
-./client 192.168.1.29 9000
+#./client.elf 192.168.1.29 9000
+
+#test tcp client using the broadcast ip
+./client.elf 192.168.8.255 9000
--- 4-tcp_udp/5-udp_prog/basic/2/run_client.sh
+++ 4-tcp_udp/5-udp_prog/basic/2/run_client.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
 #./client 192.168.1.100 9000
-./client.elf 192.168.0.129 9000
+#./client.elf 192.168.0.129 9000
 #./client.elf 192.168.8.121 9000
+
+
+#broadcast ip
+./client.elf 192.168.8.255 9000
