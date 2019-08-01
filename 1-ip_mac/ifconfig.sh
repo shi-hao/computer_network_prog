e71959780ba5c04318658e9e46613531809cfb8a
bleach <shi7631470@163.com>
2019-08-01 14:04:54 +0800

    add

--- 1-ip_mac/ifconfig.sh
+++ 1-ip_mac/ifconfig.sh
@@ -1,24 +1,37 @@
 #!/bin/bash 
 
-# ip mask gw 
-ip_array=("11.15.244.63" "192.168.1.192")
+# interface
+my_inter="enp0s31f6"
+
+# ip mask gateway array 
+ip_array=("11.15.244.63" "192.168.8.192")
 mask_array=("255.255.255.0" "255.255.255.0")
-gw_ip=("11.15.244.254" "192.168.1.1")
+gw_ip=("11.15.244.254" "192.168.8.1")
+msg=("白石桥AS" "local")
 
-len=${#ip_array[*]}
+len=$((${#ip_array[*]}-1))
 
-echo "please inpute 0-$len to choose the ip config"
-echo "ip  : ${ip_array[*]}"
-echo "mask: ${mask_array[*]}"
-echo "gw  : ${gw_ip[*]}"
+echo -en "ip \t mask \t gw \t info \n"
+for((i=0;i<=len;i++));
+do
+	echo -en "${ip_array[i]} \t  ${mask_array[i]} \t ${gw_ip[i]} \t  ${msg[i]} \n"
+done 
 
+echo -e "\033[31m please inpute 0-$len to choose the ip config\033[0m"
 read num 
-
 echo "choosing ip:${ip_array[$num]}  mask:${mask_array[$num]}  gw:${gw_ip[$num]}"
 
-#config the interface ip using cmd
-sudo ifconfig enp0s31f6 down
-sudo ifconfig enp0s31f6 11.15.244.63 netmask 255.255.255.0 
-sudo route add default gw 11.15.244.254  enp0s31f6
-sudo ifconfig enp0s31f6 up
+my_ip=${ip_array[$num]}
+my_mask=${mask_array[$num]}
+my_gw=${gw_ip[$num]}
 
+#config the interface ip using cmd
+sudo ifconfig $my_inter down
+sudo ifconfig $my_inter $my_ip netmask $my_mask
+sudo route add default gw  $my_gw  $my_inter
+sudo ifconfig $my_inter up
+
+#checkt the interface config
+ifconfig $my_inter
+route -n
+ping $my_gw
