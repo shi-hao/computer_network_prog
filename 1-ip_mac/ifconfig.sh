0a1bef8e5339b5becb38e961bd27f2a3511bca9b
bleach <shi7631470@163.com>
2019-08-02 11:32:38 +0800

    add

--- 1-ip_mac/ifconfig.sh
+++ 1-ip_mac/ifconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-# interface
-#my_inter="enp0s31f6"
-my_inter="enx000ec6be0723"
+# Interface
+my_inter="enp0s31f6"
+#my_inter="enx000ec6be0723"
 
 ## ip mask gateway array 
 #ip_array=("11.15.244.63" "192.168.8.192")
@@ -12,7 +12,7 @@ my_inter="enx000ec6be0723"
 #
 #len=$((${#ip_array[*]}-1))
 
-# read ip mask gateway
+# Read ip mask gateway
 OLD_IFS=$IFS
 IFS=" "
 while read line
@@ -26,14 +26,14 @@ done <./ip_cnf.txt
 IFS=$OLD_IFS
 len=$((${#ip_array[*]}-1))
 
-#echo all ip mask gw
+# Echo all ip mask gw
 echo -en "ip \t mask \t gw \t info \n"
 for((i=0;i<=len;i++));
 do
 	echo -en "${ip_array[i]} \t  ${mask_array[i]} \t ${gw_ip[i]} \t  ${msg[i]} \n"
 done 
 
-#choosing config
+# Choosing config
 echo -e "\033[31m please inpute 0~$len to choose the ip config\033[0m"
 read num 
 echo "choosing ip:${ip_array[$num]}  mask:${mask_array[$num]}  gw:${gw_ip[$num]}"
@@ -43,14 +43,14 @@ my_ip=${ip_array[$num]}
 my_mask=${mask_array[$num]}
 my_gw=${gw_ip[$num]}
 
-#config the interface ip using cmd
+# Config the interface ip using cmd
 sudo ifconfig $my_inter down
 sudo ifconfig $my_inter $my_ip netmask $my_mask
 sudo route del default $my_inter
-#sudo route add default gw  $my_gw  $my_inter
+sudo route add default gw  $my_gw  $my_inter
 #sudo ifconfig $my_inter up
 
-#checkt the interface config
+# Check the interface config
 ifconfig $my_inter
 route -n
 ping $my_gw
