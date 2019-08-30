3e5874535071f6cc9b794393ce129e3225a431c8
bleach <shi7631470@163.com>
2019-08-30 15:26:17 +0800

    edit

--- 6-linux_network/net_tools/ping.sh
+++ 6-linux_network/net_tools/ping.sh
@@ -38,16 +38,16 @@ do
 	for((i=0;i<=len;i++));
 	do
 		outstr=$(ping -c 1 ${ip_array[i]})
-		if [ $? -eq 1 ]; then
-			echo "------------------------------------------------"
-			echo -e "\033[34m $outstr \033[0m"
-			echo -e "\033[31m ${msg[i]}--${ip_array[i]} ping faild \033[0m"
-			echo -e "\a" #alarm 
-		else
+		if [ $? -eq 0 ]; then
 			echo "------------------------------------------------"
 			echo -e "\033[37m $outstr \033[0m"
 			echo -e "\033[32m ${msg[i]}--${ip_array[i]} alive \033[0m"
 			sleep $sleep_s
+		else
+			echo "------------------------------------------------"
+			echo -e "\033[34m $outstr \033[0m"
+			echo -e "\033[31m ${msg[i]}--${ip_array[i]} ping faild \033[0m"
+			echo -e "\a" #alarm 
 		fi
 	done 
 done
