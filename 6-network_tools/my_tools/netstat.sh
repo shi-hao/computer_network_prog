d6d4cd9c9d1da8e947f92ae4e8d6d33c4a80b287
shi-hao <shi7631470@163.com>
2019-10-31 13:30:30 +0800

    edit

--- 6-network_tools/my_tools/netstat.sh
+++ 6-network_tools/my_tools/netstat.sh
@@ -1,22 +1,41 @@
 #!/bin/bash 
 
+pause(){
+	while true
+	do
+		isContinue='y'
+		read -p 'continue? [y/n]' isContinue
+		if [ "$isContinue" == "y" ]; then
+			break
+		fi
+	done
+}
+
 while read line
 do  
 eval "$line"  
 done < des_ip.txt  
 
+echo "----------------------------------"
 echo "目标主机ip为$host_ip"
 echo "目标主机网关为$gateway"
-echo "目标主机TCP端口为$tcp_port"
-echo "目标主机UDP端口为$udp_port"
+echo "目标主机traceroute协议为$tracert_pro"
+echo "目标主机TCP端口为$nmap_tcp_port"
+echo "目标主机UDP端口为$nmap_udp_port"
+echo "----------------------------------"
 
-while true
-do
-	isContinue='y'
-	read -p 'continue? [y/n]' isContinue
-	echo $isContinue
-	
-	if [ $isContinue == 'y' ]; then
-		break
-	fi
-done
+pause
+
+echo "step1：查看本地网卡和路由配置"
+
+ifconfig 
+route -n
+
+pause
+
+echo "step2：ping主机"
+if [ -n "$host_ip" ];
+then
+	ping  -c 5 $host_ip
+	traceroute  $tracert_pro  $host_ip
+fi
