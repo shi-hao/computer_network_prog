9bc0d59f6f080c435c0099837f3edd0a564c63be
bleach <shi7631470@163.com>
2019-11-01 11:22:50 +0800

    edit

--- 6-network_tools/my_tools/netstat.sh
+++ 6-network_tools/my_tools/netstat.sh
@@ -5,7 +5,8 @@ pause(){
 	while true
 	do
 		isContinue='y'
-		read -p '请输入y继续，输入n退出 [y/n]' isContinue
+		echo -e "\033[31m 请输入y继续，输入n退出 [y/n] \033[0m"
+		read isContinue
 		if [ "$isContinue" == "y" ]; then
 			break
 		elif [ "$isContinue" == "n" ]; then
@@ -17,9 +18,9 @@ pause(){
 while read line
 do  
 eval "$line"  
-done < des_ip.txt  
+done < /home/bleach/myfile/des_ip.txt  
 
-echo "--------------------------------------------------------------------"
+echo -e "\033[31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[31m 配置文件信息：\033[0m"
 echo -e "\033[32m 
 		目标主机ip：$host_ip  
@@ -33,43 +34,48 @@ echo -e "\033[32m
 
 pause
 
-echo "--------------------------------------------------------------------"
+echo -e "\033[31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[31m step1：查看本地网卡和路由配置\033[0m"
 
 #ifconfig $local_card 
 ifconfig
+echo -e "\n-----------------------------\n"
 route -n
 
 pause
 
-echo "--------------------------------------------------------------------"
+echo -e "\033[31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[31m step2：网络连通测试\033[0m"
 if [ -n "$gateway" ];
 then
 	ping  -c 5 $gateway
+	echo -e "\n-----------------------------\n"
 	sudo traceroute  $tracert_pro  $gateway
 fi
 
 if [ -n "$host_ip" ];
 then
 	ping  -c 5 $host_ip
+	echo -e "\n-----------------------------\n"
 	sudo traceroute  $tracert_pro  $host_ip
 fi
 
 pause
 
-echo "--------------------------------------------------------------------"
+echo -e "\033[31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[31m step3：目标主机端口测试\033[0m"
 
 if [ -n "$nmap_tcp_port" ];
 then
 	sudo nmap -sS -p $nmap_tcp_port  $host_ip
+	echo -e "\n-----------------------------\n"
 fi
 
 if [ -n "$nmap_udp_port" ];
 then
 	sudo nmap -sU -p $nmap_udp_port  $host_ip
+	echo -e "\n-----------------------------\n"
 fi
 
-echo "--------------------------------------------------------------------"
+echo -e "\033[31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[31m step4：抓包查看协议数据测试\033[0m"
