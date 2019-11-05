5b6f252634130498667c28797770b2547367102e
shi-hao <shi7631470@163.com>
2019-11-05 09:19:30 +0800

    edit

--- 6-network_tools/my_tools/netstat.sh
+++ 6-network_tools/my_tools/netstat.sh
@@ -16,18 +16,44 @@ pause(){
 }
 
 # chose the config file ???
-path=/home/bleach/myfile
-all_cnf_files=$(ls -p  $path |	grep -v / | grep .txt )
-echo $all_cnf_files
+cnf_file_pattern=.cnf
+path=/home/bleach/myfile/
+all_cnf_files=$(ls -p  $path |	grep -v / | grep "$cnf_file_pattern$")
+
+files_arr=($all_cnf_files)
+len=$((${#files_arr[*]}-1))
+
+if [ $len -ge  0 ]; then
+	echo -e "find $[len+1] config file\n"
+else 
+	echo -e "  find config file failed, exit \n  config pattern $cnf_file_pattern, \n  config path is $path"
+	exit
+fi
+
+printf "%-5s %-20s\n" "ID" "file"
+for((i=0;i<=len;i++));
+do
+	printf "%-5s %-20s\n" $i ${files_arr[i]}
+done 
+
+# Chosing config files
+echo -e "\033[31m please inpute 0~$len to chose the config file\033[0m"
+read num 
+
+# target file
+target_file=${files_arr[$num]}
+echo -e "\033[31m interface:$my_inter \033[0m"
 
 while read line
 do  
 eval "$line"  
-done < /home/bleach/myfile/des_ip.txt  
+done < $path$target_file 
 
 echo -e "\033[31m--------------------------------------------------------------------\033[0m"
 echo -e "\033[31m 配置文件信息：\033[0m"
 echo -e "\033[32m 
+		说明信息：$about_info		
+
 		目标主机ip：$host_ip  
 		目标主机网关：$gateway 
 
@@ -44,7 +70,7 @@ echo -e "\033[31m step1：查看本地网卡和路由配置\033[0m"
 
 #ifconfig $local_card 
 ifconfig
-echo -e "\n-----------------------------\n"
+echo -e "\n=================================\n"
 route -n
 
 pause
@@ -54,14 +80,14 @@ echo -e "\033[31m step2：网络连通测试\033[0m"
 if [ -n "$gateway" ];
 then
 	ping  -c 5 $gateway
-	echo -e "\n-----------------------------\n"
+	echo -e "\n=================================\n"
 	sudo traceroute  $tracert_pro  $gateway
 fi
 
 if [ -n "$host_ip" ];
 then
 	ping  -c 5 $host_ip
-	echo -e "\n-----------------------------\n"
+	echo -e "\n=================================\n"
 	sudo traceroute  $tracert_pro  $host_ip
 fi
 
@@ -73,13 +99,13 @@ echo -e "\033[31m step3：目标主机端口测试\033[0m"
 if [ -n "$nmap_tcp_port" ];
 then
 	sudo nmap -sS -p $nmap_tcp_port  $host_ip
-	echo -e "\n-----------------------------\n"
+	echo -e "\n=================================\n"
 fi
 
 if [ -n "$nmap_udp_port" ];
 then
 	sudo nmap -sU -p $nmap_udp_port  $host_ip
-	echo -e "\n-----------------------------\n"
+	echo -e "\n=================================\n"
 fi
 
 echo -e "\033[31m--------------------------------------------------------------------\033[0m"
