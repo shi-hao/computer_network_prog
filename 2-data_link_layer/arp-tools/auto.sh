41afd06fc5f1d4d512216886207bd08a7a8ae660
bleach <shi7631470@163.com>
2020-06-14 02:09:09 +0800

    edit

--- 2-data_link_layer/arp-tools/auto.sh
+++ 2-data_link_layer/arp-tools/auto.sh
@@ -1,11 +1,14 @@
 #arp request
-#sudo ./arptool.elf -q -i wlp4s0 -Sip 192.168.0.1  -Tip 192.168.0.120
+#sudo ./arptool.elf -q -i wlp4s0 -Tip 192.168.31.1
 
 #arp reply
-#sudo ./arptool.elf -p -i wlp4s0  -Tip 192.168.0.120
+sudo ./arptool.elf -p -i wlp4s0  -Tip 192.168.0.120
 
 #arp reply
 #sudo ./arptool.elf -p -i wlp4s0  -Tip 192.168.0.120 -Tmac 00:00:00:00:00:00
 
 #arp reply
-sudo ./arptool.elf -p -i wlp4s0  -Tip 192.168.31.1
+#sudo ./arptool.elf -p -i wlp4s0  -Tip 192.168.31.1
+
+#arp reply
+#sudo ./arptool.elf -p -i wlp4s0  -Sip 192.168.31.1 -Tip 192.168.31.1 -Tmac 00:00:00:00:00:00
